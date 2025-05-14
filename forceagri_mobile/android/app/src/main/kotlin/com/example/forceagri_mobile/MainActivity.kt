64061ff9f3a9974dda0661c3a8169b6e0aa11bfb
Francois Botes <fbotes@nm.na>
2025-05-14 11:02:26 +0200

    second initial app update

---
+++ forceagri_mobile/android/app/src/main/kotlin/com/example/forceagri_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.forceagri_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- forceagrimobile/android/app/src/main/kotlin/com/example/forceagrimobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.forceagrimobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
